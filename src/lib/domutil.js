01aff9d2a16c2b3224057676409c211044553346
[3기]이원희 <ireba4478@gmail.com>
2020-11-03 10:30:17 +0900

    feat: domutil 리뷰 반영
    - 함수별로 export하도록 변경
    - helper함수 사용하는 로직 수정
    - attachEvent에서 detachEvent사용하도록 변경

--- src/lib/domutil.js
+++ src/lib/domutil.js
@@ -1,82 +1,76 @@
-import helper from '@/lib/helper';
+import {getElement, isString, isEmptyString, isFunction} from '@/lib/helper';
 
-const domutil = (function () {
-  let addEvent = null;
-  let removeEvent = null;
-  let element = null;
-  let listeners = [];
+let addEvent = null;
+let removeEvent = null;
+let element = null; // TODO: 초기화
+let listeners = []; // TODO: 초기화, 요소-타입별 관리
 
-  (function () {
-    // on
-    if (document.addEventListener) {
-      addEvent = function (type, handler) {
-        listeners.push(handler);
-        element.addEventListener(type, handler);
-      };
-    } else if (document.attachEvent) {
-      addEvent = function (type, handler) {
-        handler = handler.bind(element);
+(function () {
+  // on
+  if (document.addEventListener) {
+    addEvent = function (type, handler) {
+      listeners.push(handler);
+      element.addEventListener(type, handler);
+    };
+  } else if (document.attachEvent) {
+    addEvent = function (type, handler) {
+      handler = handler.bind(element);
 
-        let reverseListeners = listeners.slice().reverse();
-        reverseListeners.forEach((fn) => element.removeEventListener(type, fn));
+      const reverseListeners = listeners.slice().reverse();
+      reverseListeners.forEach((fn) => element.detachEvent(type, fn));
 
-        listeners.push(handler);
-        reverseListeners.unshift(handler);
+      listeners.push(handler);
+      reverseListeners.unshift(handler);
 
-        reverseListeners.forEach((fn) => element.attachEvent(type, fn));
-      };
-    } else {
-      addEvent = function (type, handler) {
-        handler = handler.bind(element);
-        listeners.push(handler);
-        element[`on${type}`] = (e) => listeners.forEach((fn) => fn(e));
-      };
-    }
-
-    // off
-    if (document.removeEventListener) {
-      removeEvent = function (type, handler) {
-        element.removeEventListener(type, handler);
-      };
-    } else if (document.detachEvent) {
-      removeEvent = function (type, handler) {
-        element.detachEvent(type, handler);
-      };
-    } else {
-      removeEvent = function (type, handler) {
-        listeners = listeners.filter((fn) => fn !== handler);
-        element[`on${type}`] = (e) => listeners.forEach((fn) => fn(e));
-      };
-    }
-  })();
-
-  function on(selector, type, handler) {
-    element = helper.getElement(selector);
-    helper.checkType(type);
-    helper.checkHandler(handler);
-    addEvent(type, handler);
+      reverseListeners.forEach((fn) => element.attachEvent(type, fn));
+    };
+  } else {
+    addEvent = function (type, handler) {
+      handler = handler.bind(element);
+      listeners.push(handler);
+      element[`on${type}`] = (e) => listeners.forEach((fn) => fn(e));
+    };
   }
-  function off(selector, type, handler) {
-    element = helper.getElement(selector);
-    helper.checkType(type);
-    helper.checkHandler(handler);
-    removeEvent(type, handler);
+
+  // off
+  if (document.removeEventListener) {
+    removeEvent = function (type, handler) {
+      element.removeEventListener(type, handler);
+    };
+  } else if (document.detachEvent) {
+    removeEvent = function (type, handler) {
+      element.detachEvent(type, handler);
+    };
+  } else {
+    removeEvent = function (type, handler) {
+      listeners = listeners.filter((fn) => fn !== handler);
+      element[`on${type}`] = (e) => listeners.forEach((fn) => fn(e));
+    };
   }
-  function removeAll(selector, type) {
-    element = helper.getElement(selector);
-    helper.checkType(type);
-    listeners.forEach((fn) => removeEvent(type, fn));
-  }
-  return {
-    on(selector, type, handler) {
-      on(selector, type, handler);
-    },
-    off(selector, type, handler) {
-      off(selector, type, handler);
-    },
-    removeAll(selector, type) {
-      removeAll(selector, type);
-    }
-  };
 })();
-export default domutil;
+
+export function on(selector, type, handler) {
+  element = getElement(selector);
+
+  if (!isString(type) || isEmptyString(type)) throw Error('올바른 이벤트 타입이 필요합니다.');
+  if (!isFunction(handler)) throw Error('올바른 이벤트 핸들러 함수가 필요합니다.');
+
+  addEvent(type, handler);
+}
+
+export function off(selector, type, handler) {
+  element = getElement(selector);
+
+  if (!isString(type) || isEmptyString(type)) throw Error('올바른 이벤트 타입이 필요합니다.');
+  if (!isFunction(handler)) throw Error('올바른 이벤트 핸들러 함수가 필요합니다.');
+
+  removeEvent(type, handler);
+}
+
+export function removeAll(selector, type) {
+  element = getElement(selector);
+
+  if (!isString(type) || isEmptyString(type)) throw Error('올바른 이벤트 타입이 필요합니다.');
+
+  listeners.forEach((fn) => removeEvent(type, fn));
+}
