4768a8636c2c33c58573f99ad7a442f7d35e30e4
[3기]이원희 <ireba4478@gmail.com>
2020-11-04 11:27:05 +0900

    feat: TC 작성

--- __test__/defineClass.test.js
+++ __test__/defineClass.test.js
@@ -1,79 +1,178 @@
 import util from '@/lib/util';
 
-let obj = null;
-let objWithInit = null;
-
-beforeAll(() => {
-  obj = {
-    sum: function (a, b) {
-      return a + b;
-    }
-  };
-  objWithInit = {
-    init: function (a, b) {
-      this.a = a;
-      this.b = b;
-    },
-    sum: function () {
-      return this.a + this.b;
-    }
-  };
-});
+const Sub = {
+  sum: function (a, b) {
+    return a + b;
+  }
+};
+const SubWithInit = {
+  init: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum: function () {
+    return this.a + this.b;
+  }
+};
+const Super = {
+  sum: function (a, b, c) {
+    return a + b + c;
+  },
+  multi: function (a, b, c) {
+    return a * b * c;
+  }
+};
+const SuperWithInit = {
+  init: function () {
+    this.c = 10;
+  },
+  sum: function () {
+    return this.a + this.b + this.c;
+  },
+  multi: function () {
+    return this.a * this.b * this.c;
+  }
+};
 
 describe('인스턴스 생성', () => {
   test('생성자 함수 검사', () => {
     // given
-    const Sum = util.defineClass(obj);
+    const SubClass = util.defineClass(Sub);
 
     // when
-    const instance = new Sum();
+    const instance = new SubClass();
 
     // then
-    expect(Sum.prototype.constructor).toEqual(Sum);
-    expect(instance.constructor).toEqual(Sum);
+    expect(SubClass.prototype.constructor).toEqual(SubClass);
+    expect(instance.constructor).toEqual(SubClass);
   });
 
   test('메서드 검사', () => {
     // given
-    const Sum = util.defineClass(obj);
+    const SubClass = util.defineClass(Sub);
 
     // when
-    const instance = new Sum();
+    const instance = new SubClass();
 
     // then
     expect(instance.sum(1, 2)).toBe(3);
   });
 
-  test('init 있는 경우', () => {
-    // given
-    const Sum = util.defineClass(objWithInit);
-
-    // when
-    const instance = new Sum(1, 2);
-
-    // then
-    expect(Sum.prototype.constructor).toEqual(objWithInit.init);
-    expect(instance.sum()).toBe(3);
-  });
-
   test('인스턴스가 여러개인 경우', () => {
     // given
-    const Sum = util.defineClass(objWithInit);
+    const SubClass = util.defineClass(SubWithInit);
 
     // when
-    const instance1 = new Sum(1, 2);
-    const instance2 = new Sum(1, 2);
+    const instance1 = new SubClass(1, 2);
+    const instance2 = new SubClass(1, 2);
 
     // then
     expect(instance1.sum === instance2.sum).toBeTruthy();
     expect(instance1 === instance2).toBeFalsy();
   });
+
+  describe('매개변수를 전달하는 init 있는 경우', () => {
+    test('instance type 검사', () => {
+      // given
+      const SubClass = util.defineClass(SubWithInit);
+
+      // when
+      const instance = new SubClass(1, 3);
+
+      // then
+      expect(instance instanceof SubWithInit.init).toBeTruthy();
+    });
+
+    test('멤버 변수 검사', () => {
+      // given
+      const SubClass = util.defineClass(SubWithInit);
+
+      // when
+      const instance = new SubClass(1, 3);
+
+      // then
+      expect(instance.a).toBe(1);
+      expect(instance.b).toBe(3);
+    });
+  });
 });
 
-describe('부모 상속 인스턴스 생성', () => {
-  test('인스턴스 ', () => {
+describe('부모 클래스를 상속한 인스턴스 생성', () => {
+  test('생성자 함수 검사', () => {
     // given
+    const SubClass = util.defineClass(Sub, Super);
+
     // when
+    const instance = new SubClass();
+
     // then
+    expect(SubClass.prototype.constructor).toEqual(SubClass);
+    expect(instance.constructor).toEqual(SubClass);
+  });
+
+  test('메서드 오버라이딩 검사', () => {
+    // given
+    const SubClass = util.defineClass(Sub, Super);
+
+    // when
+    const instance = new SubClass();
+
+    // then
+    expect(instance.sum(1, 3, 5)).toBe(4); // 1 + 3
+    expect(instance.sum(1, 3, 5)).not.toBe(9); // 1 + 3 + 5
+    expect(instance.multi(1, 3, 5)).toBe(15); // 1 * 3 * 5
+  });
+
+  test('인스턴스가 여러개인 경우', () => {
+    // given
+    const SubClass = util.defineClass(SubWithInit, SuperWithInit);
+
+    // when
+    const instance1 = new SubClass(1, 2);
+    const instance2 = new SubClass(1, 2);
+
+    // then
+    expect(instance1.sum === instance2.sum).toBeTruthy();
+    expect(instance1.multi === instance2.multi).toBeTruthy();
+    expect(instance1 === instance2).toBeFalsy();
+  });
+
+  describe('매개변수를 전달하는 init 있는 경우', () => {
+    test('instance type 검사', () => {
+      // given;
+      const SubClass = util.defineClass(SubWithInit, SuperWithInit);
+
+      // when
+      const instance = new SubClass(1, 3);
+
+      // then
+      expect(instance instanceof SubWithInit.init).toBeTruthy();
+      expect(instance instanceof SuperWithInit.init).toBeTruthy();
+    });
+
+    test('멤버 변수 검사', () => {
+      // given;
+      const SubClass = util.defineClass(SubWithInit, SuperWithInit);
+
+      // when
+      const instance = new SubClass(1, 3);
+
+      // then
+      expect(instance.a).toBe(1);
+      expect(instance.b).toBe(3);
+      expect(instance.c).toBe(10);
+    });
+
+    test('메소드 오버라이딩 검사', () => {
+      // given;
+      const SubClass = util.defineClass(SubWithInit, SuperWithInit);
+
+      // when
+      const instance = new SubClass(1, 3);
+
+      // then
+      expect(instance.sum()).toBe(4); // 1 + 3
+      expect(instance.multi()).toBe(30); // 1 * 3 * 10
+    });
   });
 });
