edc07c0a5e3d49cd3f1f3de40c8b4524163c42d2
이원희 <wonhee.lee@nhntoast.com>
2020-11-06 15:27:24 +0900

    test: sample 화면 css 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 import dnd from '@/lib/dnd';
 import * as domutil from '@/lib/domutil';
+import '@/styles/index.scss';
 const str = `
-    <input value="button" type="button" style="width:50px;height:30px"/>      
-    <div id="red" class="red" style="margin-top:20px;background-color: red;width:100px;height:100px;">
-        <input value="button" type="button2" style="width:50px;height:30px;background-color:yellow"/>      
+    <div class="item"></div>   
+    <div class="list">
     </div>
 `;
 document.body.innerHTML = str;
 
-dnd.draggable('input');
+dnd.draggable('.item');
 
-const dropzone = dnd.dropzone('#red');
+const dropzone = dnd.dropzone('.list');
 
 domutil.on(dropzone, 'drop', (eventData) => {
   console.log(eventData);
