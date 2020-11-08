dde9e4504e67338d192ed862fdbe230c43f4b4bf
이원희 <wonhee.lee@nhntoast.com>
2020-11-08 21:50:54 +0900

    feat: sample code 수정

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,12 @@ const str = `
     <div class="item">1</div>   
     <div class="item">2</div>   
     <div class="item">3</div>   
-    <div class="item">4</div>   
-    <div class="list">
+    <div class="item">4</div>
+    <div style="display:flex">
+      <div class="list">
+      </div>
+      <div class="list2">
+      </div>
     </div>
 `;
 document.body.innerHTML = str;
@@ -15,7 +19,11 @@ document.body.innerHTML = str;
 document.querySelectorAll('.item').forEach((el) => dnd.draggable(el));
 
 const dropzone = dnd.dropzone('.list');
+const dropzone2 = dnd.dropzone('.list2');
 
 domutil.on(dropzone, 'drop', (eventData) => {
-  console.log('eventData : ', eventData.detail);
+  console.log('eventData(dropzone) : ', eventData.detail);
+});
+domutil.on(dropzone2, 'drop', (eventData) => {
+  console.log('eventData(dropzone2) : ', eventData.detail);
 });
